7d3b13d1b90ed4e33ee554d35bceda43b778d688
Homero Cavazos <homerocavazos@gmail.com>
2020-05-03 16:51:01 -0500

    Updated init

--- _js/jsa.js
+++ _js/jsa.js
@@ -2,84 +2,82 @@ const jsa = (function () {
   let settings = {
     dt: "dt",
     dd: "dd",
-  }
+  };
 
-  const dt = document.querySelectorAll(settings.dt)
-  const dd = document.querySelectorAll(settings.dd)
+  const dt = document.querySelectorAll(settings.dt);
+  const dd = document.querySelectorAll(settings.dd);
 
   const _terms = Object.keys(dt).map(function (e) {
-    return dt[e]
-  })
+    return dt[e];
+  });
   const _definitions = Object.keys(dd).map(function (e) {
-    return dd[e]
-  })
+    return dd[e];
+  });
 
   const _resetDefinitions = () => {
     //Reset Term
     _terms.map(function (term) {
-      term.classList.remove("active")
-    }, 0)
+      term.classList.remove("active");
+    }, 0);
     // Reset Definitions
     _definitions.map(function (definition) {
-      definition.classList.add("collapsed")
-      definition.classList.remove("show")
-    }, 0)
-  }
+      definition.classList.add("collapsed");
+      definition.classList.remove("show");
+    }, 0);
+  };
 
   const _collapse = (el) => {
     // Add active to clicked item
     // This takes care of the plus minus icon
-    let term = el
-    term.classList.add("active")
+    let term = el;
+    term.classList.add("active");
     // Show the DD
-    let definition = el.nextElementSibling
-    definition.classList.remove("collapsed")
-    definition.classList.add("show")
-  }
+    let definition = el.nextElementSibling;
+    definition.classList.remove("collapsed");
+    definition.classList.add("show");
+  };
 
   const _logic = (e) => {
-    e.preventDefault()
-    let el = e.target
+    e.preventDefault();
+    let el = e.target;
     if (el.classList.value === "active") {
-      el.classList.remove("active")
-      _resetDefinitions()
+      el.classList.remove("active");
+      _resetDefinitions();
     } else if (e.keyCode === 13) {
       // Enter
-      _resetDefinitions()
-      _collapse(el)
+      _resetDefinitions();
+      _collapse(el);
     } else if (e.keyCode === 9) {
       // Tab
-      el.blur()
+      el.blur();
     } else {
-      _resetDefinitions()
-      _collapse(el)
+      _resetDefinitions();
+      _collapse(el);
     }
-  }
+  };
 
   const _init = () => {
     _definitions.reduce((index, definition) => {
-      definition.classList.add("collapsed")
-      definition.setAttribute("id", "definition" + index)
-      definition.setAttribute("aria-labelledby", "term" + index)
-      index += 1
-      return index
-    }, 1)
+      definition.classList.add("collapsed");
+      definition.setAttribute("id", "definition" + index);
+      definition.setAttribute("aria-labelledby", "term" + index);
+      index += 1;
+      return index;
+    }, 1);
 
     _terms.reduce((index, term) => {
-      term.addEventListener("click", _logic)
-      term.addEventListener("keydown", _logic)
-      term.setAttribute("id", "term" + index)
-      term.setAttribute("data-target", "definition" + index)
-      term.setAttribute("tabindex", "0")
-      index += 1
-      return index
-    }, 1)
-  }
+      term.addEventListener("click", _logic);
+      term.addEventListener("keydown", _logic);
+      term.setAttribute("id", "term" + index);
+      term.setAttribute("data-target", "definition" + index);
+      term.setAttribute("tabindex", "0");
+      index += 1;
+      return index;
+    }, 1);
+  };
 
   // Public API
   return {
     init: _init,
-  }
-})() //jsa
-
-jsa.init()
+  };
+})(); //jsa
