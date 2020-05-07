557dd9b871af766da374702e737a5477cde060f1
Homero Cavazos <homerocavazos@gmail.com>
2020-05-07 09:58:07 -0500

    added missing reference

--- _js/jsa.1.2.js
+++ _js/jsa.1.2.js
@@ -11,110 +11,111 @@ __________________________________________
  Website: https://github.com/homerocavazos/jsa
 
  */
-"use strict";
-let jsa = window.jsa || {};
+"use strict"
+let jsa = window.jsa || {}
 
 jsa = (() => {
   function jsa() {
-    const _ = this;
+    const _ = this
 
     _.settings = {
       dt: "a",
       dd: "dd",
-    };
-    _.el = null;
+    }
+    _.el = null
   } //jsa function
 
-  return jsa;
-})();
+  return jsa
+})()
 
 jsa.prototype.setOpts = function (opts) {
+  let _ = this
   if (typeof opts == "object")
     for (let key in opts) {
       if (opts.hasOwnProperty(key)) {
-        _.settings[key] = opts[key];
+        _.settings[key] = opts[key]
       }
     }
-  else return;
-};
+  else return
+}
 jsa.prototype.setParentEL = function (el) {
-  let _ = this;
-  _.el = el || ".jsa";
-};
+  let _ = this
+  _.el = el || ".jsa"
+}
 jsa.prototype.getID = function (el) {
-  let _ = this;
-  let id = _.el.replace(".", "");
-  return id;
-};
+  let _ = this
+  let id = _.el.replace(".", "")
+  return id
+}
 jsa.prototype.getObjs = function (objs) {
   return Object.keys(objs).map(function (e) {
-    return objs[e];
-  });
-};
+    return objs[e]
+  })
+}
 
 jsa.prototype.reset = function () {
-  let _ = this;
+  let _ = this
   _.terms.map(function (term) {
-    term.classList.remove("active");
-  });
+    term.classList.remove("active")
+  })
   _.definitions.map(function (definition) {
-    definition.classList.add("collapsed");
-    definition.classList.remove("show");
-  });
-};
+    definition.classList.add("collapsed")
+    definition.classList.remove("show")
+  })
+}
 
 jsa.prototype.collapse = function (el) {
-  el.classList.add("active");
+  el.classList.add("active")
 
-  let triggerID = el.dataset.target;
-  let targetDefinition = document.getElementById(triggerID);
+  let triggerID = el.dataset.target
+  let targetDefinition = document.getElementById(triggerID)
 
-  targetDefinition.classList.remove("collapsed");
-  targetDefinition.classList.add("show");
-};
+  targetDefinition.classList.remove("collapsed")
+  targetDefinition.classList.add("show")
+}
 
 jsa.prototype.logic = function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  let _ = this; // this jsa obj
-  let el = e.target;
+  let _ = this // this jsa obj
+  let el = e.target
 
   if (el.classList.value === "active") {
-    el.classList.remove("active");
-    _.reset();
-    el.blur();
+    el.classList.remove("active")
+    _.reset()
+    el.blur()
   } else {
-    _.reset();
-    _.collapse(el);
-    el.blur();
+    _.reset()
+    _.collapse(el)
+    el.blur()
   }
-};
+}
 jsa.prototype.init = function (el, opts) {
-  let _ = this;
-  _.setParentEL(el);
-  _.setOpts(opts);
+  let _ = this
+  _.setParentEL(el)
+  _.setOpts(opts)
 
-  _.terms = _.getObjs(document.querySelectorAll(`${_.el} ${_.settings.dt}`));
+  _.terms = _.getObjs(document.querySelectorAll(`${_.el} ${_.settings.dt}`))
 
   _.terms.map(function (a) {
-    a.addEventListener("click", _.logic.bind(_));
-  });
+    a.addEventListener("click", _.logic.bind(_))
+  })
 
   _.definitions = _.getObjs(
     document.querySelectorAll(`${_.el} ${_.settings.dd}`)
-  );
+  )
   _.definitions.reduce((index, definition) => {
-    definition.classList.add("collapsed");
-    definition.setAttribute("id", `${_.getID()}-definition` + index);
-    definition.setAttribute("aria-labelledby", `${_.getID()}-term` + index);
-    index += 1;
-    return index;
-  }, 1);
+    definition.classList.add("collapsed")
+    definition.setAttribute("id", `${_.getID()}-definition` + index)
+    definition.setAttribute("aria-labelledby", `${_.getID()}-term` + index)
+    index += 1
+    return index
+  }, 1)
   _.terms.reduce((index, term) => {
-    term.setAttribute("id", `${_.getID()}-term` + index);
-    term.setAttribute("data-target", `${_.getID()}-definition` + index);
-    term.setAttribute("tabindex", "0");
-    index += 1;
-    return index;
-  }, 1);
-};
+    term.setAttribute("id", `${_.getID()}-term` + index)
+    term.setAttribute("data-target", `${_.getID()}-definition` + index)
+    term.setAttribute("tabindex", "0")
+    index += 1
+    return index
+  }, 1)
+}
